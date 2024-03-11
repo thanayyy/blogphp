0bd4614a9e95b262f3c8217ca017b67c6af991bb
thanayyy <thanayyy5998@gmail.com>
2024-03-11 17:42:19 +0700

    .

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -46,7 +46,7 @@ adminOnly();
                         <tr>
                             <td><?php echo $key + 1; ?></td>
                             <td><?php echo $post['title'] ?></td>
-                            <td>Thana</td>
+                            <td><?php echo $_SESSION['username'];?></td>
                             <td><a href="edit.php?id=<?php echo $post['id']; ?>" class="edit">edit</a></td>
                             <td><a href="edit.php?delete_id=<?php echo $post['id']; ?>" class="delete">delete</a></td>
                             <?php if($post['published']): ?>
